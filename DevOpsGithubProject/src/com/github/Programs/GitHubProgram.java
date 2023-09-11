2c55606b3ae88dd43fbd94a7cfb2c8436e401e17
SP.Shah <SP.Shah@XAV-602000238.TI.ads>
2023-09-11 21:07:03 +0530

    Cloning New Project

---
+++ DevOpsGithubProject/src/com/github/Programs/GitHubProgram.java
@@ -0,0 +1,12 @@
+package com.github.Programs;
+
+public class GitHubProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Program Code created by TI account on 11/09/2023 T 05:40 PM");
+
+	}
+
+}
